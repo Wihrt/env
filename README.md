6e8fbd5d6247466d91803883e431e030cd16e0d2
Arnaud Hatzenbuhler <ahatzenbuhler@solocal.com>
2022-12-20 12:02:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# devenv
+
+Use devenv to setup easily the environment
+Responsability of devenv :
+Install tools
+
+Scripts = Give aliases
+
+Use pre-commit.yaml file to load all the pre-commit
+process = update pre-commit hooks
+
---
+++ devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "toto";
+
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.docker
+    pkgs.docker-client
+    pkgs.docker-compose
+    pkgs.git
+    pkgs.jq
+    pkgs.lazydocker
+    pkgs.p7zip
+    pkgs.pre-commit  # We prefer to install the package pre-commit instead of using devenv pre-commit
+    pkgs.unzip
+    pkgs.vscode
+    pkgs.yq
+    pkgs.zip
+    pkgs.zsh
+  ];
+
+  # https://devenv.sh/languages/
+  #languages.nix.enable = true;
+  languages.python.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.pc.exec = "pre-commit"
+
+
+  # https://devenv.sh/processes/
+  processes.docker-compose.exec = "docker-compose up -d";
+  processes.pre-commit.exec = "pre-commit install --install-hooks";
+}
+
