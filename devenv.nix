026b8b0a33130d668c7e6a7290271ae63210180b
Arnaud Hatzenbuhler <ahatzenbuhler@solocal.com>
2022-12-28 17:22:03 +0100

    Composing with multiple devenv And it works ! Even with private repos (you still need access)

--- devenv.nix
+++ devenv.nix
@@ -7,33 +7,6 @@
 
   # https://devenv.sh/packages/
   packages = [
-    pkgs.docker
-    pkgs.docker-client
-    pkgs.docker-compose
-    pkgs.git
-    pkgs.jq
-    pkgs.lazydocker
-    pkgs.p7zip
-    pkgs.pre-commit  # We prefer to install the package pre-commit instead of using devenv pre-commit
-    pkgs.unzip
-    pkgs.vscode
-    pkgs.yq
-    pkgs.zip
-    pkgs.zsh
+    pkgs.mysql
   ];
-
-  enterShell = ''
-    pci
-  '';
-
-  # https://devenv.sh/languages/
-  #languages.nix.enable = true;
-  languages.python.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.pci.exec = ''pre-commit install --install-hooks'';
-  scripts.pca.exec = ''pre-commit autoupdate'';
-
-  # https://devenv.sh/processes/
-  processes.docker-compose.exec = "docker-compose up -d";
 }
